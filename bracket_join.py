849960ce511a11806c5e3fb956ca494d7688a9f0
enuaj <169210307+yelloowww@users.noreply.github.com>
2024-10-03 23:45:10 +0200

    Bracket join: Add two joins
    * 4 x Bracket/16-2Q + Bracket/8 -> Bracket/64
    * 4 x Bracket/16-2Q -> Bracket/64 (incomplete)

--- bracket_join.py
+++ bracket_join.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Callable
 
 import wikitextparser as wtp
 
@@ -6,58 +7,124 @@ import wikitextparser as wtp
 @dataclass
 class Join:
     new_name: str
-    changes: tuple[dict[str, str], dict[str, str]]
+    original_template_changes: dict[str, str]
+    other_template_changes: dict[int | tuple[int, ...], dict[str, Callable]]
+    reorder: tuple[str] | None = None
 
 
 JOINS = {
-    "Bracket/32": {
-        "Bracket/16L8DSL4DSL2DSL1D": Join(
-            new_name="Bracket/32U16L8DSL4DSL2DSL1D",
-            changes=(
-                {
-                    **{f"R3M{x}": f"R4M{x}" for x in range(1, 5)},
-                    **{f"R4M{x}": f"R6M{x}" for x in range(1, 3)},
-                    "R5M1": "R8M1",
-                },
-                {
-                    **{f"R1M{x}": f"R1M{x + 16}" for x in range(1, 9)},
-                    **{f"R2M{x}": f"R2M{x + 8}" for x in range(1, 9)},
-                    **{f"R4M{x}": f"R4M{x + 4}" for x in range(1, 5)},
-                    **{f"R6M{x}": f"R6M{x + 2}" for x in range(1, 3)},
-                    "R8M1": "R8M2",
-                },
-            ),
-        )
-    }
+    ("Bracket/32", "Bracket/16L8DSL4DSL2DSL1D"): Join(
+        new_name="Bracket/32U16L8DSL4DSL2DSL1D",
+        original_template_changes={
+            **{f"R3M{x}": f"R4M{x}" for x in range(1, 5)},
+            **{f"R4M{x}": f"R6M{x}" for x in range(1, 3)},
+            "R5M1": "R8M1",
+        },
+        other_template_changes={
+            1: {
+                **{f"R1M{x}": f"R1M{x + 16}" for x in range(1, 9)},
+                **{f"R2M{x}": f"R2M{x + 8}" for x in range(1, 9)},
+                **{f"R4M{x}": f"R4M{x + 4}" for x in range(1, 5)},
+                **{f"R6M{x}": f"R6M{x + 2}" for x in range(1, 3)},
+                "R8M1": "R8M2",
+            },
+        },
+    ),
+    ("Bracket/16-2Q", "Bracket/16-2Q", "Bracket/16-2Q", "Bracket/16-2Q", "Bracket/8"): Join(
+        new_name="Bracket/64",
+        original_template_changes={
+            "qualifiedHeader": None,
+        },
+        other_template_changes={
+            **{
+                i: {
+                    **{f"R1M{x}": f"R1M{x + i * 8}" for x in range(1, 9)},
+                    **{f"R2M{x}": f"R2M{x + i * 4}" for x in range(1, 5)},
+                    **{f"R3M{x}": f"R3M{x + i * 2}" for x in range(1, 3)},
+                }
+                for i in range(1, 4)
+            },
+            4: {
+                "R1M1header": "R4M1header",
+                "R2M1header": "R5M1header",
+                "R3M1header": "R6M1header",
+                **{f"R1M{x}": f"R4M{x}" for x in range(1, 5)},
+                **{f"R2M{x}": f"R5M{x}" for x in range(1, 3)},
+                "R3M1": "R6M1",
+            },
+        },
+        reorder=(
+            *(f"R{r}M1header" for r in range(1, 7)),
+            *(f"R{r}M{x}" for r in range(1, 7) for x in range(1, 2 ** (6 - r) + 1)),
+        ),
+    ),
+    ("Bracket/16-2Q", "Bracket/16-2Q", "Bracket/16-2Q", "Bracket/16-2Q"): Join(
+        new_name="Bracket/64",
+        original_template_changes={
+            "qualifiedHeader": None,
+        },
+        other_template_changes={
+            **{
+                i: {
+                    **{f"R1M{x}": f"R1M{x + i * 8}" for x in range(1, 9)},
+                    **{f"R2M{x}": f"R2M{x + i * 4}" for x in range(1, 5)},
+                    **{f"R3M{x}": f"R3M{x + i * 2}" for x in range(1, 3)},
+                }
+                for i in range(1, 4)
+            }
+        },
+        reorder=(
+            *(f"R{r}M1header" for r in range(1, 7)),
+            *(f"R{r}M{x}" for r in range(1, 4) for x in range(1, 2 ** (6 - r) + 1)),
+        ),
+    ),
 }
 
 
 def bracket_join(original: str) -> str:
     parsed = wtp.parse(original)
 
-    upper_tpl: wtp.Template | None = None
-    upper_tpl_name: str = ""
-
-    for tpl in parsed.templates:
-        name = tpl.normal_name(capitalize=True)
-        if name == "Bracket" and (x := tpl.get_arg("1")):
-            bracket_tpl_name = x.value.strip()
-            if bracket_tpl_name in JOINS:
-                upper_tpl = tpl
-                upper_tpl_name = bracket_tpl_name
-            elif upper_tpl and bracket_tpl_name in JOINS[upper_tpl_name]:
-                join = JOINS[upper_tpl_name][bracket_tpl_name]
-                upper_tpl.set_arg("1", join.new_name)
-                upper_changes, lower_changes = join.changes
-                for arg_from, arg_to in upper_changes.items():
-                    if x := upper_tpl.get_arg(arg_from):
-                        x.name = arg_to
-                for arg in tpl.arguments:
-                    if arg.name in lower_changes:
-                        upper_tpl.set_arg(lower_changes[arg.name], arg.value)
-                    elif not upper_tpl.has_arg(arg.name):
-                        upper_tpl.set_arg(arg.name, arg.value)
+    brackets = [tpl for tpl in parsed.templates if tpl.normal_name(capitalize=True) == "Bracket" and tpl.get_arg("1")]
+    while len(brackets) > 1:
+        bracket_names = [tpl.get_arg("1").value.strip() for tpl in brackets]
+        for names, join in JOINS.items():
+            if tuple(bracket_names[: len(names)]) == names:
+                print("GOING IN", bracket_names[: len(names)], names)
+                apply_join(join, brackets[: len(names)])
+                brackets = brackets[len(names) :]
+                print("DONE", len(brackets))
+        else:
+            brackets = brackets[1:]
 
     converted = str(parsed)
 
     return converted
+
+
+def apply_join(join: Join, brackets: list[wtp.Template]) -> None:
+    brackets[0].set_arg("1", join.new_name)
+
+    for arg_from, arg_to in join.original_template_changes.items():
+        if x := brackets[0].get_arg(arg_from):
+            if arg_to is None:
+                brackets[0].del_arg(arg_from)
+            else:
+                x.name = arg_to
+
+    for i, bracket in enumerate(brackets[1:], start=1):
+        if changes := join.other_template_changes.get(i):
+            for arg in bracket.arguments:
+                if arg.name in changes:
+                    arg_to = changes[arg.name]
+                    if isinstance(arg_to, Callable):
+                        arg_to = arg_to(i)
+                    brackets[0].set_arg(arg_to, arg.value)
+                elif not brackets[0].has_arg(arg.name):
+                    brackets[0].set_arg(arg.name, arg.value)
+
+    if join.reorder:
+        for arg in join.reorder:
+            if x := brackets[0].get_arg(arg):
+                val = x.value
+                brackets[0].del_arg(arg)
+                brackets[0].set_arg(arg, val)
